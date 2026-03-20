bc756518179d7c45bdfdd9a5d749b60ca71fc275
ViniSFerraz <viniciussferrazoficial@gmail.com>
2026-03-19 22:30:09 -0300

    aula 2 variaveis e operadores

---
+++ Aula-2-variaveis-e-operadores/aula02a-variaveis.py
@@ -0,0 +1,26 @@
+print("Olá Mundo")
+
+print(7 + 4)
+print("7 + 4")
+print("7" + "4")
+
+# Cometário de 1 linha
+'''
+Cometários de
+multiplas linhas
+'''
+
+# VARIÁVEIS
+nome = "Vinícius" #string
+idade = 17 #int
+peso = 65.1 #float
+
+print(nome, idade, peso)
+print(f"Hello,{nome}!")
+
+# IMPUTS - SUMULAÇÃO DE FORMS NO CMD
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+print(nome, idade)
+print(idade + 1)
